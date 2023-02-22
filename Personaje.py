e17d70f5a133b54c4394e43d973a1d4f5703518b
Monset-L <121037975@upq.edu.mx>
2023-02-22 17:06:09 -0600

    Pedimos datos para el constructor y colocamos métodos

--- Personaje.py
+++ Personaje.py
@@ -1,22 +1,24 @@
 class Personaje:
-    #atributos Personaje
-    especie= "Humano"
-    nombre= "Master Chief"
-    altura= "2.70"
+    #Definimos el constructor de personaje
+    def __init__(self,esp,nom,alt):
+        self.especie= esp
+        self.nombre= nom
+        self.altura= alt
+    
 
     #Metodos Personaje
 
     def correr(self,status):
         if(status):
-            print("El personaje"+ self.nombre +"esta corriendo")
+            print("El personaje "+ self.nombre +" esta corriendo")
         else:
-            print("El personaje"+ self.nombre +"se detuvo")
+            print("El personaje "+ self.nombre +"cse detuvo")
 
     def lanzarGranadas(self):
-        print("El personaje"+ self.nombre +"lanzo una granada")
+        print("El personaje "+ self.nombre +" lanzo una granada")
 
     def recargarArma(self, municiones):
         cargador= 10
         cargador= cargador + municiones
-        print("El arma tiene"+ str(cargador) +"balas")
+        print("El arma tiene "+ str(cargador) +" balas")
     
--- objetos.py
+++ objetos.py
@@ -1,18 +1,47 @@
 
 from Personaje import *
 
-#1.Crear objeto de clase Personaje
+#1.Solicitar datos
 
-heroe= Personaje()
+print("")
+print("######### Datos Heroe ########")
+especieH=input("Escribe la especie del Heroe: ")
+nombreH=input("Escribe el nombre del Heroe: ")
+alturaH=float(input("Escribe la altura del Heroe: "))
+recargarH=int(input("Cuantas balas recargas al Heroe: "))
 
-#2.Usar atributos
+print("")
+print("######### Datos Villano ########")
+especieV=input("Escribe la especie del Villano: ")
+nombreV=input("Escribe el nombre del Villano: ")
+alturaV=float(input("Escribe la altura del Villano: "))
+recargarV=int(input("Cuantas balas recargas al Villano: "))
 
+#2.Crear objeto de clase Personaje
+
+heroe= Personaje(especieH,nombreH,alturaH)
+villano= Personaje(especieV,nombreV,alturaV)
+
+#3.Usar atributos
+
+print("")
+print("######### Objeto Heroe ########")
 print("El personaje se llama: "+ heroe.nombre)
-print("pertenece a la especie: "+ heroe.especie)
-print("y tiene una altura de: "+ heroe.altura)
+print("Pertenece a la especie: "+ heroe.especie)
+print("y tiene una altura de: "+ str(heroe.altura))
 
-#3.Usar metodos
-
-heroe.correr(True)
+heroe.correr(False)
 heroe.lanzarGranadas()
-heroe.recargarArma(87)
+heroe.recargarArma(recargarV)
+
+
+print("")
+print("######### Objeto Villano ########")
+print("El personaje se llama: "+ villano.nombre)
+print("Pertenece a la especie: "+ villano.especie)
+print("y tiene una altura de: "+ str(villano.altura))
+
+
+villano.correr(False)
+villano.lanzarGranadas()
+villano.recargarArma(recargarV)
