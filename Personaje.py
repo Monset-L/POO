3626c90009884f724729209c8e0d77d31f2ce1ac
Monset-L <121037975@upq.edu.mx>
2023-02-24 16:47:35 -0600

    Encapsulamiento de los atributos
    Declaramos los getters y setters para agregarlos a nuestros atributos

--- Personaje.py
+++ Personaje.py
@@ -1,24 +1,44 @@
 class Personaje:
     #Definimos el constructor de personaje
     def __init__(self,esp,nom,alt):
-        self.especie= esp
-        self.nombre= nom
-        self.altura= alt
+        self.__especie= esp
+        self.__nombre= nom
+        self.__altura= alt
     
 
     #Metodos Personaje
 
     def correr(self,status):
         if(status):
-            print("El personaje "+ self.nombre +" esta corriendo")
+            print("El personaje "+ self.__nombre +" esta corriendo")
         else:
-            print("El personaje "+ self.nombre +"cse detuvo")
+            print("El personaje "+ self.__nombre +"cse detuvo")
 
     def lanzarGranadas(self):
-        print("El personaje "+ self.nombre +" lanzo una granada")
+        print("El personaje "+ self.__nombre +" lanzo una granada")
 
     def recargarArma(self, municiones):
         cargador= 10
         cargador= cargador + municiones
         print("El arma tiene "+ str(cargador) +" balas")
+    
+    def __pensar(self):                                           #Agregar una función de pensar y tiene que ser privado
+        print("Estoy pensando.................")
+    
+    #Declarar Getters y Setter de atributos
+    
+    def getNombre(self):
+        return self.__nombre
+    def setNombre(self,nom):
+        self.__nombre= nom
+        
+    def getEspecie(self):
+        return self.__especie
+    def setEspecie(self,esp):
+        self.__especie= esp
+        
+    def getAltura(self):
+        return self.__altura
+    def setAltura(self,alt):
+        self.__altura= alt    
     
--- objetos.py
+++ objetos.py
@@ -23,23 +23,25 @@ heroe= Personaje(especieH,nombreH,alturaH)
 villano= Personaje(especieV,nombreV,alturaV)
 
 #3.Usar atributos
+heroe.setNombre(" Pepe pecas ")     #Cambiar el atributo por medio del set, ignorando lo demás 
 
 print("")
 print("######### Objeto Heroe ########")
-print("El personaje se llama: "+ heroe.nombre)
-print("Pertenece a la especie: "+ heroe.especie)
-print("y tiene una altura de: "+ str(heroe.altura))
+print("El personaje se llama: "+ heroe.getNombre())
+print("Pertenece a la especie: "+ heroe.getEspecie())
+print("y tiene una altura de: "+ str(heroe.getAltura()))
 
 heroe.correr(False)
 heroe.lanzarGranadas()
 heroe.recargarArma(recargarV)
+heroe.__pensar()
 
 
 print("")
 print("######### Objeto Villano ########")
-print("El personaje se llama: "+ villano.nombre)
-print("Pertenece a la especie: "+ villano.especie)
-print("y tiene una altura de: "+ str(villano.altura))
+print("El personaje se llama: "+ villano.getNombre())
+print("Pertenece a la especie: "+ villano.getEspecie())
+print("y tiene una altura de: "+ str(villano.getAltura()))
 
 
 villano.correr(False)
