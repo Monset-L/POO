80d4297b4e3856e2542e56ed0d3d60f8cd0107dc
Monset-L <121037975@upq.edu.mx>
2023-03-24 17:11:37 -0600

    Realización final del insert en SQLite

--- tkinterSQLite/UsuariosCRUD.py
+++ tkinterSQLite/UsuariosCRUD.py
@@ -1,6 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
+from controladorBD import * #Le presentamos la clase a la ventana
+
+#Crear un objeto de tipo controlador
+controlador= controladorDB()
+
+#Proceder a guardar usando el metodo del objeto controlador
+def ejecutainsert():
+    controlador.guardarUsuario(varNom.get(), varCor.get(), varCon.get())
+
 
 Ventana= Tk()
 Ventana.title("CRUD Usuarios")
@@ -28,7 +37,7 @@ varCon= tk.StringVar()
 lblCon= Label(pestana1,text="Contraseña: ").pack()
 txtCon= Entry(pestana1,textvariable=varCon).pack()
 
-btnGuardar=Button(pestana1,text="Guardar usuario").pack()
+btnGuardar=Button(pestana1,text="Guardar usuario",command=ejecutainsert).pack()
 
 panel.add(pestana1,text="Formulario de usuarios")
 panel.add(pestana2,text="Buscar usuario")
--- tkinterSQLite/controladorBD.py
+++ tkinterSQLite/controladorBD.py
@@ -1,3 +1,49 @@
 from tkinter import messagebox
 import sqlite3
 import bcrypt
+
+class controladorDB:
+    def __init__(self):
+        pass
+    
+    #1. Preparamos la conexión para usarla cuando sea necesario
+    def conexionBD(self):
+        try:
+            conexion= sqlite3.connect("C:/Users/lupit/Documents/uni/Cuatri 5/POO/github/POO/tkinterSQLite/DBUsuarios.db")   
+            print("Conectado BD") 
+            return conexion
+        
+        except sqlite3.OperationalError:
+            print("No se pudo conectar")
+            
+            
+    def guardarUsuario(self, nom, cor, con):
+        
+        #1. Llamar a la conexión 
+        conx= self.conexionBD()
+        
+        #2. Revisar que los parámetros no estén vacios
+        if(nom == "" or cor=="" or con==""):
+            messagebox.showwarning("Aguas!!", "Revisa tu formulario")
+            conx.close()
+        else:
+            #3. Insertar,Preparamos los datos y el querySQL
+            cursor= conx.cursor()
+            conH= self.encryptarCon(con)
+            datos=(nom,cor,conH)
+            qrInsert="insert into TBRegistrados(nombre, correo, contraseña)values(?,?,?) "
+            
+            #4. Proceder a insertar y cerramos la conexión
+            cursor.execute(qrInsert, datos)
+            conx.commit()
+            conx.close()
+            messagebox.showinfo("Éxito","Se guardó el usuario")
+            
+    def encryptarCon (self,con):
+        conPlana= con
+        conPlana= conPlana.encode()  #Convertimos con a bytes
+        sal= bcrypt.gensalt()
+        conHa= bcrypt.hashpw(conPlana, sal)
+        
+        print(conHa)
+        return conHa
