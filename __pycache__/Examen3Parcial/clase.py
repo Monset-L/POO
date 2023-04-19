9b7ede5da207e7d0373fec2a210f35a47321c53f
Monset-L <121037975@upq.edu.mx>
2023-04-19 17:13:27 -0600

    3er Commit
    Funciones de eliminar y consultar casi terminadas

--- __pycache__/Examen3Parcial/clase.py
+++ __pycache__/Examen3Parcial/clase.py
@@ -31,3 +31,43 @@ class claseDB:
             conx.close()
             messagebox.showinfo("Éxito","Se guardaron los datos")
     
+    def EliminarDatos(self,id):
+        conx =self.conexionBD()
+        if(id == ""):
+            messagebox.showwarning("Cuidado", "Id vacio escribe uno valido")
+            conx.close()
+        else:
+            try:
+                cursor = conx.cursor()
+                sqlDelte = ("DELETE FROM TBPedimentos WHERE IDExpo = ?")
+                dato=(id)
+                Mensaje= messagebox.askokcancel("Cuidado", "Está seguro de que desea eliminar los datos?")
+                if Mensaje == True:
+                    cursor.execute(sqlDelte,dato)
+                    conx.commit()
+                    conx.close()
+                    messagebox.showinfo("Eliminado","Se eliminaron los datos")
+                else:
+                    conx.close()
+                    messagebox.showerror("Algo falló","Ningún dato eliminado")
+            
+            except sqlite3.OperationalError:
+                    print("Error Consulta")
+                    
+    
+    def consultaDato(self,Adu):
+        conx= self.conexionBD()
+        if(Adu == ""):
+            messagebox.showwarning("Cuidado", "Campo vacio escribe uno valido")
+            conx.close()
+        else:
+            try:
+                cursor= conx.cursor()
+                sqlSelect="select * from TBPedimentos where Aduana="+Adu
+                
+                cursor.execute(sqlSelect)
+                RSusuario = cursor.fetchall()
+                conx.close()
+                return RSusuario
+            except sqlite3.OperationalError:
+                print("Error Consulta")
--- __pycache__/Examen3Parcial/ventana.py
+++ __pycache__/Examen3Parcial/ventana.py
@@ -7,6 +7,25 @@ controlador= claseDB()
 
 def ejecutainsert():
     controlador.insertarDatos(varTra.get(),varAdu.get())
+    
+def ejecutarEliminar(): 
+    controlador.EliminarDatos(Varid.get())
+    
+def ejecutaConsultaA():
+    for i in textBu.get_children():
+        textBu.delete(i)
+        
+    rsUsuario=controlador.consultaDato(varCon.get())
+    
+    for usu in rsUsuario:
+        
+        cadena= str(usu[0])+" "+usu[1]+" "+usu[2]+" "+str(usu[3]) #concatenar par hacerla cadena
+        
+    if(rsUsuario):
+        textBu.insert("",0,values=cadena)
+    
+    else:
+        messagebox.showinfo("Aduana no encontrado","Dato no registrado en BD")
 
 Ventana= Tk()
 Ventana.title("Exportaciones")
@@ -33,13 +52,13 @@ btnGuardar=Button(pestana1,text="Guardar datos",command=ejecutainsert).pack()
 #Pestaña 2 Eliminar
 titulo5=Label(pestana2, text="Eliminar Datos",fg="green",font=("Modern",18)).pack()
 
-VarElid=tk.StringVar()
-Eid=Label(pestana2,text='ID: ')
-Eid.place(x=160,y=40)
-Elid= tk.Entry(pestana2,textvariable=VarElid)
-Elid.place(x=220,y=40)
+Varid=tk.StringVar()
+Elid=Label(pestana2,text='ID: ')
+Elid.place(x=160,y=40)
+Elmid= tk.Entry(pestana2,textvariable=Varid)
+Elmid.place(x=220,y=40)
 
-btnEliminar=Button(pestana2,text="Eliminar")
+btnEliminar=Button(pestana2,text="Eliminar",command=ejecutarEliminar())
 btnEliminar.place(x=220, y=80)
 
 
@@ -48,7 +67,28 @@ panel.add(pestana2,text="Eliminar Datos")
 panel.add(pestana3,text="Consultar Datos por Aduana")
 
 
+#Pestaña 3 Consultar Aduana
+titulo2=Label(pestana3, text="Consultar Aduana",fg="green",font=("Modern",18)).pack()
 
+varCon=tk.StringVar()
+iblAdu=Label(pestana3, text="Identificador de Usuario: ").pack()
+txtAdu= Entry(pestana3,textvariable=varCon).pack()
+
+btnBusqueda=Button(pestana3,text="Consultar",command=ejecutaConsultaA()).pack()
+
+subBus=Label(pestana3,text="Registrados:",fg="blue",font=("Modern",15) ).pack()
+textBu=ttk.Treeview(pestana3,columns=('#0','#1','#2','#3'))
+textBu.heading("#1",text="ID",anchor=CENTER)
+textBu.heading("#2",text="Transporte",anchor=CENTER)
+textBu.heading("#3",text="Aduana",anchor=CENTER)
+
+
+textBu.column("#0",width=1)
+textBu.column("#1",width=20)
+textBu.column("#2",width=100)
+textBu.column("#3",width=150)
+
+textBu.pack()         
 
 
 
